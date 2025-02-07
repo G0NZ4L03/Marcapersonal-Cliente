22519e537d3a806eb48176a3e1fe6242487c43eb
Gonzalo <1832888@alu.murciaeduca.es>
2025-02-07 10:30:09 +0100

    Base del proyecto con todo instalado

---
+++ webGonzalo/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Base trabajo</h1>
+      <h2>marcapersonal</h2>
+    </>
+  )
+}
+
+export default App
